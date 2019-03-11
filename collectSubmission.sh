27f56275d2d571306a445c4d9cdbce35dcd681e7
Nader Namini <nadernamini@berkeley.edu>
2019-03-10 19:37:20 -0700

    added new submission script [Does not work]

--- collectSubmission.sh
+++ collectSubmission.sh
@@ -15,4 +15,4 @@ done
 
 
 rm -f assignment2.zip
-zip -r assignment2.zip . -x "*.git" "*deeplearning/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements*.txt" ".env/*" "*.pyc" "*deeplearning/build/*"
+zip -r assignment2.zip . -x "*.git*" "*.idea*" "*deeplearning/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements*.txt" ".env/*" "*.pyc" "*deeplearning/build/*"
